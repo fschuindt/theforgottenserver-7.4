c6807a291fdfbb6aeaf7d3900175bafa392dcddf
Seliro <emil92b2@gmail.com>
2016-09-07 23:25:41 +0200

    energy beam

--- server/data/spells/scripts/attack/energy beam.lua
+++ server/data/spells/scripts/attack/energy beam.lua
@@ -1,6 +1,6 @@
-local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ENERGYHIT)
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_EXPLOSIONHIT)
 
 local area = createCombatArea(AREA_BEAM5, AREADIAGONAL_BEAM5)
 setCombatArea(combat, area)
